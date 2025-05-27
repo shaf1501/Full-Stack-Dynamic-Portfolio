5c050b24dd7997fafa0e1a1b14b932a63d6607a0
shaf1501 <shafin15-4903@diu.edu.bd>
2025-05-27 17:30:23 +0600

    basic_portfolio_design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Navbar Only</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <style>
+    .navbar {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 15px;
+    }
+    .logo {
+      font-weight: bold;
+      font-size: 20px;
+    }
+    .nav_link {
+      display: flex;
+      gap: 15px;
+    }
+    .nav_item {
+      text-decoration: none;
+      color: black;
+      font-size: 16px;
+    }
+    .a {
+      border: 1px solid black;
+      padding: 5px 10px;
+      border-radius: 5px;
+    }
+  </style>
 </head>
 <body>
-    <h1>
-        Welcome from Laravel App!
-    </h1> 
+
+  <div class="navbar">
+    <div class="logo">Tanjid Ahammed Shafin</div>
+    <div class="nav_link">
+      <div class="nav_item a">Home</div>
+      <div class="nav_item a">About</div>
+      <div class="nav_item a">Projects</div>
+      <div class="nav_item a">Skills</div>
+      <div class="nav_item a">Contact</div>
+    </div>
+  </div>
+
 </body>
 </html>
